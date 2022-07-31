d33d4b04042e23b8ff24e660502994139027212e
max <maxwellbezerra@live.com>
2022-07-30 22:20:39 -0200

    Implemented may stuffs on compose. Finished project. Can improve in future,but its done.

--- app/src/main/java/com/example/mealzcompose/ui/details/MealDetailsScreen.kt
+++ app/src/main/java/com/example/mealzcompose/ui/details/MealDetailsScreen.kt
@@ -1,24 +1,30 @@
 package com.example.mealzcompose.ui.details
 
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.animateDp
-import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import coil.compose.AsyncImage
 import com.example.mealzcompose.model.response.MealResponse
+import java.lang.Float.min
 
 
 //var profilePictureState by remember { mutableStateOf(MealProfilePictureState.Normal) }
@@ -43,36 +49,48 @@ import com.example.mealzcompose.model.response.MealResponse
 @Composable
 fun MealDetailsScreen(meal: MealResponse?) {
 
+    val scrollState = rememberLazyListState()
+    val offset = min(1f, 1 -
+            (scrollState.firstVisibleItemScrollOffset / 600f
+                    * scrollState.firstVisibleItemIndex))
+    val size by animateDpAsState(targetValue = max(100.dp, 148.dp * offset))
 
     Surface(color = MaterialTheme.colors.background) {
 
         Column {
-            Row {
-                Card(
-                    modifier = Modifier.padding(16.dp),
-                    shape = CircleShape,
-                    border = BorderStroke(
-                        width = 2.dp,
-                        color = Color.Green
-                    )
-                ) {
-                    AsyncImage(
-                        model = meal?.imageUrl,
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop,
+            Surface(elevation = 4.dp) {
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Card(
+                        modifier = Modifier.padding(16.dp),
+                        shape = CircleShape,
+                        border = BorderStroke(
+                            width = 2.dp,
+                            color = Color.Green
+                        )
+                    ) {
+                        AsyncImage(
+                            model = meal?.imageUrl,
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(size)
+                        )
+                    }
+                    Text(
+                        text = meal?.name ?: "default name",
                         modifier = Modifier
-                            .size(200.dp)
-                            .padding(8.dp)
+                            .padding(16.dp)
+                            .align(Alignment.CenterVertically)
                     )
                 }
-                Text(
-                    text = meal?.name ?: "default name",
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .align(Alignment.CenterVertically)
-                )
             }
+            val dummyContentList = (0..100).map { it.toString() }
+            LazyColumn(state = scrollState) {
+                items(dummyContentList) { dummyItem ->
+                    Text(text = dummyItem, modifier = Modifier.padding(24.dp))
 
+                }
+            }
         }
     }
 }
